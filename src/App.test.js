6e32cdbf6a8e18ea485d4a977e08a14d09dc46a4
Muhammad Aftab Hameed <muhammad.hameed@glaston.net>
2023-08-30 00:42:37 +0300

    # New theme integrated

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@
 
 // test('renders learn react link', () => {
 //   render(<App />);
-//   const linkElement = screen.getByText(/muhammad aftab hameed/i);
+//   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
 
